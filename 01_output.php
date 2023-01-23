cf1bf29a51052da1bc0b9ec093e1c6f029ecaef4
alain-alda <alain.antisocial@gmail.com>
2023-01-23 10:43:20 +0800

    My First PHP

---
+++ 01_output.php
@@ -0,0 +1,24 @@
+<?php
+// echo "Hello World!!!";
+// echo "<br>";
+// echo 123;
+
+echo "<h1>Hello World!</h1>";
+
+// print is similar to echo
+
+// print "hello";
+// print_r('hello');
+// print "<br>";
+
+// $fname = "Ryan"; // String can be in single or double quotes
+// $age = "41"; // Integer
+
+// echo "$fname is $age years old";
+
+// print_r([1,2,3]);
+
+// var_dump - More info data type and length
+// var_dump ("Hello");
+// var_dump([1,2,6]);
+?>
